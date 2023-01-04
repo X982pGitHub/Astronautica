c07cfdddafb43f0014cf768699330828e6c45161
X982pGitHub <thee.thitut@gmail.com>
2023-01-04 16:12:02 +0700

    featured article

---
+++ js/home.js
@@ -0,0 +1,10 @@
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function updateSize() {
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+}
+
+updateSize();
+window.addEventListener("resize", updateSize);
