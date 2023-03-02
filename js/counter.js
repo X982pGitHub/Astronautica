6c86ff0f6ddb2cdd25d3a3871241375e576a5bf7
X982p <thee.thitut@gmail.com>
2023-03-03 02:15:10 +0700

    update page hit to log view time

--- js/counter.js
+++ js/counter.js
@@ -1,8 +1,36 @@
-window.addEventListener('load', function() {
-    var pageName = document.location.pathname.split('/').pop().replace('.html', '');
+let pageName, enterTime;
+function getEnterTime(){
+    pageName = document.location.pathname.split('/').pop().replace('.html', '');
+    enterTime = Date.now();
+    console.log("Enter at: " + new Date(enterTime));
+}
+
+async function getLeaveTime(){
+    let leaveTime = Date.now(); 
+    console.log("Leave at: " + new Date(leaveTime)); 
+    return leaveTime;
+}
+
+async function getDuration(){
+    let duration = ((await getLeaveTime()) - enterTime); 
+    console.log("Duration: " + duration/1000 + " seconds");
     // Send the hit data to the server
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open('POST', '/pagehit');
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({pageName: pageName}));
+    xhr.send(JSON.stringify({pageName: pageName, viewTime: duration}));
+};
+
+window.addEventListener('load', () => {
+    getEnterTime();
+});
+window.addEventListener('beforeunload', () => {
+    getDuration();
+});
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible"){
+        getEnterTime();
+    } else {
+        getDuration();
+    }
 });
